512cd29973a0b8f8328be0cca4ab4a9be49f3310
Thiago <thiago-fa@hotmail.com>
2023-01-21 20:44:53 -0300

    acertad6 a

--- js/script.js
+++ js/script.js
@@ -12,9 +12,35 @@ texto.addEventListener('keypress', function (e) {
 })
 
 function validainput(texto) {
-    const padrao = '[a-z]';
+    const padrao = '[a-z ]';
 
     if (texto.match(padrao)) {
         return true;
     }
+}
+
+function criptografar() {
+
+    if (texto.value !== "") {
+
+        let campoCriptografado = document.querySelector('.ladoBtext')
+        const imagem = document.querySelector('.ladoBpadrao');
+
+        if (campoCriptografado.classList.contains('displaynone')) {
+
+            for (child of imagem.children) {
+                child.classList.add('displaynone')
+            }
+
+            campoCriptografado.classList.remove('displaynone')
+            campoCriptografado.classList.add('displayYes')
+
+        } else {
+            console.log('Deu errado')
+        }
+
+    } else {
+        alert("Digite um texto para ser criptografado")
+    }
+
 }
